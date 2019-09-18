d662398edff068afec693ff6ffad776f9178bf81
sealot <sun_e@sina.com>
2019-09-18 11:21:57 +0800

    '20190918'

---
+++ getip.py
@@ -0,0 +1,26 @@
+import requests
+import regex
+
+
+
+session=requests.session()
+
+response1=session.get("http://ip138.com/")
+html1=response1.text
+
+reg1=r"<iframe src=\"(http:\/\/[\S]+)\" [\s\S]+>[\s\S]*<\/iframe>"
+
+pattern1=regex.compile(reg1)
+
+url=pattern1.findall(html1)
+response2=session.get(url[0])
+html2=response2.text
+reg2=r"<title>您的IP地址是：([\S]+)<\/title>"
+
+pattern2=regex.compile(reg2)
+
+ip=pattern2.findall(html2)
+
+
+with open('ip.txt','w') as f:
+    f.write(ip[0])
